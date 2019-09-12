cad1eca07e0014c0b8d1b2b6d43374d92a3b596a
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2019-09-12 20:43:28 -0300

    Criado o modelo Compras e configurado o arquivo admin.py

--- compras/app_compras/admin.py
+++ compras/app_compras/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class ComprasAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao', 'unidades', 'quant_mes',
+                    'preco', 'preco_max')
+
+
+admin.site.register(Compras,ComprasAdmin)
--- compras/app_compras/models.py
+++ compras/app_compras/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Compras(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=100)
+    unidades = models.IntegerField()
+    quant_mes = models.FloatField(round(2))
+    preco = models.FloatField(round(2))
+    preco_max = models.FloatField(round(2))
+
+    def __str__(self):
+        return self.nome
+
