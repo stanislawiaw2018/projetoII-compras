bb0f4a85784e503da1b405b0fbd414c1ef3e5814
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2019-09-12 22:04:02 -0300

    Alterado os atributos do campo preco_max

--- compras/app_compras/models.py
+++ compras/app_compras/models.py
@@ -5,9 +5,9 @@ class Compras(models.Model):
     nome = models.CharField(max_length=50)
     descricao = models.CharField(max_length=100)
     unidades = models.IntegerField()
-    quant_mes = models.FloatField(round(2))
-    preco = models.FloatField(round(2))
-    preco_max = models.FloatField(round(2))
+    quant_mes = models.FloatField()
+    preco = models.FloatField()
+    preco_max = models.DecimalField(max_digits=2, decimal_places=2)
 
     def __str__(self):
         return self.nome
